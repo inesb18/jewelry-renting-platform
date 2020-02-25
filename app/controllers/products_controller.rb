f945eaa240c06ad469b67e39fcf0192c08217b9d
Erin Soggs <erinasoggs@gmail.com>
2020-02-25 15:58:49 -0500

    added category column for products, required validations, adapted params, adapted creation form, and completed the seeds

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -16,6 +16,6 @@ class ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:name, :description, :price, :photo)
+    params.require(:product).permit(:name, :description, :price, :photo, :category)
   end
 end
