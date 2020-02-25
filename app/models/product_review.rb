8361531976b7608483abe5063452f33b7f8e1734
Ines Belghiti <ines.belghiti@gmail.com>
2020-02-25 11:40:47 -0500

    Added the validations for product, rental, unavailability, and the reviews

--- app/models/product_review.rb
+++ app/models/product_review.rb
@@ -1,3 +1,8 @@
 class ProductReview < ApplicationRecord
   belongs_to :rental
+  validates :content, presence: true
+  validates :rating, presence: true
+  validates :rating, numericality: { only_integer: true }, presence: true,
+                     inclusion: { in: [0, 1, 2, 3, 4, 5] }
+  validates :rental, presence: true
 end
--- app/models/rental.rb
+++ app/models/rental.rb
@@ -3,4 +3,9 @@ class Rental < ApplicationRecord
   belongs_to :product
   has_many :rentee_reviews
   has_many :product_reviews
+  validates :start_date, presence: :true
+  validates :end_date, presence: :true
+  validates :end_date, presence: true, date: { after_or_equal_to:  :start_date}
+  validates :product, presence: true
+  validates :user, presence: true
 end
