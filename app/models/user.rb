254ad1616826f6d953c567265ddaae5f91232d68
Ines Belghiti <ines.belghiti@gmail.com>
2020-02-25 10:23:56 -0500

    started working on user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_one_attached :photo
+  has_one_attached :photo, dependent: :destroy
   has_many :rentals
-  has_many :products
+  has_many :products, dependent: :destroy
+  validates :first_name, presence: true, format: { with: /\A[a-z|A-Z|\s|-|,]+\z/ }
+  validates :last_name, presence: true, format: { with: /\A[a-z|A-Z|\s|-|,]+\z/ }
+  validates :address, presence: true
 end
