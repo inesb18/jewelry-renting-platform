978da8bea01662c37013ab9e297519a84bb37fbf
Ines Belghiti <ines.belghiti@gmail.com>
2020-02-25 18:07:37 -0500

    Completed validation upon creation of rentals to check if product is available at the dates of the rental

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -3,4 +3,22 @@ class RentalsController < ApplicationController
     @product = Product.find(params[:product_id])
     @rental = Rental.new
   end
+
+  def create
+    @rental = Rental.new(rental_params)
+    @product = Product.find(params[:product_id])
+    @rental.product = @product
+    @rental.user = current_user
+    if @rental.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def rental_params
+    params.require(:rental).permit(:start_date,:end_date)
+  end
 end
--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,12 +1,26 @@
 class MyValidator < ActiveModel::Validator
   def validate(record)
     valid = true
+    un_start_date = nil
+    un_end_date = nil
     record.product.unavailabilities.each do |un|
       if record.start_date <= un.end_date && record.start_date >= un.start_date
         valid = false
+        un_start_date = un.start_date
+        un_end_date = un.end_date
+      elsif record.end_date <= un.end_date && record.end_date >= un.start_date
+        valid = false
+        un_start_date = un.start_date
+        un_end_date = un.end_date
+      elsif record.start_date < un.start_date && record.end_date > un.end_date
+        valid = false
+        un_start_date = un.start_date
+        un_end_date = un.end_date
       end
     end
-    record.errors.add(:base, "Sorry, it is unavailable. Try other dates.") unless valid
+    unless valid
+      record.errors.add(:base, "Sorry, it is unavailable from #{un_start_date.strftime('%b %d, %Y')} to #{un_end_date.strftime('%b %d, %Y')}. Try other dates.")
+    end
   end
 end
 
@@ -19,8 +33,10 @@ class Rental < ApplicationRecord
   validates :end_date, presence: :true
   validates :product, presence: true
   validates :user, presence: true
-  validate :end_after_start
+
   validate :future_dates
+  validate :end_after_start
+  validate :different_dates
 
   include ActiveModel::Validations
   validates_with MyValidator
@@ -38,5 +54,12 @@ class Rental < ApplicationRecord
       errors.add(:start_date, "must be after today")
     end
   end
+
+  def different_dates
+    if start_date == end_date
+      errors.add(:base, "The rental has to be for 24 hours minimum")
+    end
+  end
+
 end
 
