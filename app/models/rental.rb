60e58eb4cd7c0f30115648fd230f5a75b79db651
Ines Belghiti <ines.belghiti@gmail.com>
2020-02-25 12:37:21 -0500

    Fixed the validations for dates (rentals and unavailability

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -5,7 +5,15 @@ class Rental < ApplicationRecord
   has_many :product_reviews
   validates :start_date, presence: :true
   validates :end_date, presence: :true
-  validates :end_date, presence: true, date: { after_or_equal_to:  :start_date}
   validates :product, presence: true
   validates :user, presence: true
+  validate :end_after_start
+
+  def end_after_start
+  return if end_date.blank? || start_date.blank?
+
+  if end_date < start_date
+    errors.add(:end_date, "must be after the start date")
+  end
+ end
 end
