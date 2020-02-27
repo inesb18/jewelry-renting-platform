482bccb7502796caebc7361f1c9f35c9fcc4e104
Ines Belghiti <ines.belghiti@gmail.com>
2020-02-27 16:00:07 -0500

    New schema with new migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_27_155633) do
+ActiveRecord::Schema.define(version: 2020_02_27_180641) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,17 +63,18 @@ ActiveRecord::Schema.define(version: 2020_02_27_155633) do
     t.date "end_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "confirmed", default: false
     t.index ["product_id"], name: "index_rentals_on_product_id"
     t.index ["user_id"], name: "index_rentals_on_user_id"
   end
 
-  create_table "rentee_reviews", force: :cascade do |t|
+  create_table "renter_reviews", force: :cascade do |t|
     t.integer "rating"
     t.text "content"
     t.bigint "rental_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["rental_id"], name: "index_rentee_reviews_on_rental_id"
+    t.index ["rental_id"], name: "index_renter_reviews_on_rental_id"
   end
 
   create_table "unavailabilities", force: :cascade do |t|
@@ -108,6 +109,6 @@ ActiveRecord::Schema.define(version: 2020_02_27_155633) do
   add_foreign_key "products", "users"
   add_foreign_key "rentals", "products"
   add_foreign_key "rentals", "users"
-  add_foreign_key "rentee_reviews", "rentals"
+  add_foreign_key "renter_reviews", "rentals"
   add_foreign_key "unavailabilities", "products"
 end
