df394cce74617bb3e0209741e274a81c6a76c0d0
Ines Belghiti <ines.belghiti@gmail.com>
2020-02-28 11:12:27 -0500

    Added JS to ask for unregistered user location and display the nearest jewels

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,9 +1,12 @@
 import "bootstrap";
 import { updatePrice } from '../components/total_price';
 import { initAutocomplete } from '../plugins/init_autocomplete';
+import { showMeNearMe } from '../components/near_me_unregistered';
 
 console.log("Hello");
 
 updatePrice();
 
 initAutocomplete();
+
+showMeNearMe();
