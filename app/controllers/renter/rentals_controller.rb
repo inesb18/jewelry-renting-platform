4b678ced2e95ec89b7e3a47f9cfc6d44e4d452f8
Ines Belghiti <ines.belghiti@gmail.com>
2020-02-28 16:07:04 -0500

    Completed the seed for demo

--- app/controllers/renter/rentals_controller.rb
+++ app/controllers/renter/rentals_controller.rb
@@ -3,17 +3,17 @@ class Renter::RentalsController < ApplicationController
     @rentals = policy_scope(Rental).where(user: current_user)
     @renter = true
     @status = params[:status]
-    if @status == "confirmed"
-      @rentals = @rentals.select { |rental| rental.confirmed && !rental.declined }
-      @rentals = @rentals.reject { |rental| rental.end_date <= Date.today }
+    if @status == "pending"
+      @rentals = @rentals.select { |rental| !rental.declined && !rental.confirmed }
     elsif @status == "declined"
       @rentals = @rentals.select { |rental| rental.declined }
     elsif @status == "past"
       @rentals = @rentals.select { |rental| rental.confirmed && !rental.declined }
       @rentals = @rentals.select { |rental| rental.end_date < Date.today }
     else
-      @status = "pending"
-      @rentals = @rentals.select { |rental| !rental.declined && !rental.confirmed }
+      @status = "confirmed"
+      @rentals = @rentals.select { |rental| rental.confirmed && !rental.declined }
+      @rentals = @rentals.reject { |rental| rental.end_date <= Date.today }
     end
   end
 end
