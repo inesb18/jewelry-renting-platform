4d19fb2bd5e393943e5e8d52098f517d950892bd
Erin Soggs <erinasoggs@gmail.com>
2020-02-28 14:52:58 -0500

    finished being able to view user profile who is requesting rental

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+    authorize(@user)
+  end
+
   def edit
     @user = current_user
     authorize(@user)
